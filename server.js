f720441980440dc2ee5ea2d3c9f44cb7ee039684
Full Name <theofficialwebsiteguys@gmail.com>
2024-08-01 17:00:34 -0400

    adding shipping rates calc

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const crypto = require('crypto');
 const querystring = require('querystring');
 const nonce = require('nonce')();
 const request = require('request-promise');
+const { Shopify } = require('@shopify/shopify-api');
 require('dotenv').config();
 
 const app = express();
@@ -94,6 +95,60 @@ app.get('/shopify/callback', (req, res) => {
   }
 });
 
+// app.get('/shopify/callback', (req, res) => {
+//   const { shop, hmac, code, state } = req.query;
+//   const stateCookie = cookie.parse(req.headers.cookie).state;
+
+//   console.log("State: " + state);
+//   console.log("State Cookie: " + stateCookie);
+
+//   if (state !== stateCookie) {
+//     return res.status(403).send('Request origin cannot be verified');
+//   }
+
+//   if (shop && hmac && code) {
+//     const map = { ...req.query };
+//     delete map['hmac'];
+//     const message = querystring.stringify(map);
+//     const generatedHash = crypto.createHmac('sha256', process.env.SHOPIFY_API_SECRET).update(message).digest('hex');
+
+//     if (generatedHash !== hmac) {
+//       return res.status(400).send('HMAC validation failed');
+//     }
+
+//     const accessTokenRequestUrl = 'https://' + shop + '/admin/oauth/access_token';
+//     const accessTokenPayload = {
+//       client_id: process.env.SHOPIFY_API_KEY,
+//       client_secret: process.env.SHOPIFY_API_SECRET,
+//       code
+//     };
+
+//     request.post(accessTokenRequestUrl, {json: accessTokenPayload })
+//     .then((accessTokenResponse) => {
+//       const accessToken = accessTokenResponse.access_token;
+
+//       const apiRequestUrl = 'https://' + shop + '/admin/products.json';
+//       const apiRequestHeader = {
+//         'X-Shopify-Access-Token' : accessToken
+//       }
+
+//       request.get(apiRequestUrl, { headers: apiRequestHeader})
+//       .then((apiResponse) => {
+//         res.end(apiResponse);
+//       })
+//       .catch((error) => {
+//         res.status(error.statusCode).send(error.error.error_description);
+//       })
+//     })
+//     .catch((error) => {
+//       res.status(error.statusCode).send(error.error.error_description);
+//     });
+
+//   } else {
+//     res.status(400).send('Required parameters missing');
+//   }
+// });
+
 app.post('/shopify/rate', async (req, res) => {
   console.log("Received rate request body:", JSON.stringify(req.body, null, 2));
 
@@ -157,11 +212,14 @@ app.post('/shopify/rate', async (req, res) => {
   }
 });
 
-const server = app.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`App running on port ${PORT}`);
 });
 
-server.on('error', (err) => {
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}`);
+}).on('error', (err) => {
   if (err.code === 'EADDRINUSE') {
     console.error(`Port ${PORT} is already in use`);
     process.exit(1);
